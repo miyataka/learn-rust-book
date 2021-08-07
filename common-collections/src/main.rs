1d86f898d0617b22ebaa93946ac47d86dd9b5e48
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 20:56:26 +0900

    8-9: vector mutalble iterate

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let v = vec![100, 32, 57];
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
     for i in &v {
         println!("{}", i);
     }
