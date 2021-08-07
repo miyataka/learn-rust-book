54a4b842e75c97d8a662cc5a1d68c901919cef0f
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 21:01:32 +0900

    8-10: enum stored in vector

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
-    for i in &v {
-        println!("{}", i);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    for i in &row {
+        println!("{:?}", i);
     }
 }
