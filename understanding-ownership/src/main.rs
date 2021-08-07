dce223f2249f4f5df8d59e53a57a59b81e917f74
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 13:33:07 +0900

    update

--- understanding-ownership/src/main.rs
+++ understanding-ownership/src/main.rs
@@ -1,19 +1,12 @@
 fn main() {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
 
-    let s3 = takes_and_gives_back(s2);
-
-    println!("{}", s1);
-    println!("{}", s3);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
