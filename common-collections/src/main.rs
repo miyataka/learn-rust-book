5a1e1420f426cd69ab08bdda2a5fb5f4965e914d
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 21:22:48 +0900

    8-24: update HashMap

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -4,9 +4,7 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Blue"), 50);
 
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
+    println!("{:?}", scores);
 }
