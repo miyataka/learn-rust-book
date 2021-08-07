0ec0fe0703e386a8bd620c3cb435c2fd41652f7b
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 18:44:13 +0900

    7-22

--- learn-packages/src/main.rs
+++ learn-packages/src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
+use rand::Rng;
 
 fn main() {
-    let mut map = HashMap::new();
-    map.insert(1, 2);
+    let secret_number = rand::thread_rng().gen_range(1..101);
 }
