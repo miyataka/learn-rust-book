3e4f70a02a5a85b2d058184f68b72833dff2e24f
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 21:18:26 +0900

    8-22: HashMap and Ownership

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
 fn main() {
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let intial_scores = vec![10, 50];
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    let scores: HashMap<_, _> = teams.into_iter().zip(intial_scores.into_iter()).collect();
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 
-    println!("{:?}", scores);
+    println!("{:?}", map);
 }
