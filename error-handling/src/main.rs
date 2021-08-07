7b7c148a792013270c3cbfa6a13456bfb21fd187
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 21:44:47 +0900

    9-4: error handling when file open

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+
 fn main() {
-    panic!("crash and burn");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
