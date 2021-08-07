808532d527c60c6e543c8b755aadc76e2c4da97e
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 21:16:34 +0900

    8-21: HashMap

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let intial_scores = vec![10, 50];
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let scores: HashMap<_, _> = teams.into_iter().zip(intial_scores.into_iter()).collect();
 
     println!("{:?}", scores);
 }
