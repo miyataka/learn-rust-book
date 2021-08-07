973dda628c5d484efc086b8e5d2b0e81179322bd
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 18:21:56 +0900

    7-10

--- learn-packages/src/lib.rs
+++ learn-packages/src/lib.rs
@@ -37,10 +37,14 @@ mod back_of_house {
             }
         }
     }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
