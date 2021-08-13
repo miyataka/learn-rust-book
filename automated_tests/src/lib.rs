db8a655da4ff0f81c1059dc03a7e651bcbe43141
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-13 21:54:18 +0900

    assert with message

--- automated_tests/src/lib.rs
+++ automated_tests/src/lib.rs
@@ -14,6 +14,11 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    // String::from("Hello!")
+    format!("Hello {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +54,15 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was {}",
+            result
+        );
     }
 }
