68cdf54ad91aba4da8edfe2381a2290757ee30f4
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-13 21:47:48 +0900

    11-7: assert_eq! macro

--- automated_tests/src/lib.rs
+++ automated_tests/src/lib.rs
@@ -27,4 +27,27 @@ mod tests {
 
         assert!(larger.can_hold(&smaller));
     }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
