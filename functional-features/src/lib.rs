15513fbc149b663e68852bde686ddfecd5c6b8e9
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-15 18:44:30 +0900

    13-22

--- functional-features/src/lib.rs
+++ functional-features/src/lib.rs
@@ -1,3 +1,26 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(PartialEq, Debug)]
 struct Shoe {
     size: u32,
@@ -44,5 +67,17 @@ mod tests {
                 },
             ]
         );
+    }
+
+    #[test]
+    fn calling_next_directory() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
     }
 }
