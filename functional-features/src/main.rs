ea439c922ffe96e0b3526ff6308ab340a30543b7
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-15 16:48:39 +0900

    13-1: 2 seconds function

--- functional-features/src/main.rs
+++ functional-features/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+use std::time::Duration;
+
+fn main() {}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
