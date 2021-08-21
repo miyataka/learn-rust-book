1ee646c4f268ced77e774df6a0778f5f2690b11d
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-21 19:19:05 +0900

    15-6: dereference operator

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -1,10 +1,7 @@
-use List::{Cons, Nil};
-
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-}
+    let x = 5;
+    let y = &x;
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
