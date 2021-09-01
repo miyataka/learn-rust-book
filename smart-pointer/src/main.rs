7de11aa9fa6382fba5f1594bf2e5a02a4ae68f20
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-09-01 21:54:32 +0900

    15-14: Drop trait

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -9,6 +9,14 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
 
 struct MyBox<T>(T);
@@ -29,4 +37,14 @@ impl<T> Deref for MyBox<T> {
 
 fn hello(name: &str) {
     println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
